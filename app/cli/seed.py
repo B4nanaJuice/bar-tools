540336a99c73069d312abdffa6932084c789252b
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2026-09-07 21:33:48 +0200

    Added seed data

--- app/cli/seed.py
+++ app/cli/seed.py
@@ -1,6 +1,7 @@
 import click
 from flask import current_app
 from flask.cli import with_appcontext
+import json
 
 from app.database import db
 from app.models import Cocktail, CocktailIngredient, CocktailTag
@@ -9,7 +10,10 @@ from app.models import Ingredient, Tag
 @click.command("seed")
 @with_appcontext
 def seed():
-    
-    click.echo("This is the start of the seeding process...")
+
+    with open("app/cli/seed_data.json", mode = "r", encoding = "utf-8") as f:
+        data = json.load(fp = f)["cocktails"]
+
+        click.echo(f"Loading {len(data)} cocktails")
 
     click.echo("End of the process !")
