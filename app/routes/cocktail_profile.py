ebdc440f5f99a0c23deeecbf0ab1c45ec2adb521
B4nanaJuice <griesmaxime2@gmail.com>
2026-09-10 10:00:24 +0200

    Indexing cocktails by id

--- app/routes/cocktail_profile.py
+++ app/routes/cocktail_profile.py
@@ -5,9 +5,9 @@ from app.models import Cocktail
 
 page = Blueprint("cocktail_profile", __name__)
 
-@page.route("/cocktail/<string:cocktail_name>")
-def cocktail_profile(cocktail_name: str):
+@page.route("/cocktail/<int:cocktail_id>")
+def cocktail_profile(cocktail_id: int):
 
-    cocktail = db.session.scalar(db.select(Cocktail).where(Cocktail.name == cocktail_name))
+    cocktail = db.get_or_404(Cocktail, cocktail_id)
     
     return f"{cocktail.name}"
